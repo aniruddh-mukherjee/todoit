59bbecb64bab023c29b9ff9e5741ff774fc44170
aniruddh-mukherjee <aniruddha.xccentric@gmail.com>
2020-07-15 01:20:00 +0530

    Updates

---
+++ precache-manifest.980be31c2c834988f84ece751baddb84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1f07ef2a60e21960fa87d934588a3d",
+    "url": "/todoit/index.html"
+  },
+  {
+    "revision": "b47ebdb6ad4648c22690",
+    "url": "/todoit/static/css/main.36857277.chunk.css"
+  },
+  {
+    "revision": "3150ea2ec44357dd78fa",
+    "url": "/todoit/static/js/2.58a4c16e.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/todoit/static/js/2.58a4c16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47ebdb6ad4648c22690",
+    "url": "/todoit/static/js/main.16a12f9d.chunk.js"
+  },
+  {
+    "revision": "8ff5097a879499aa2552",
+    "url": "/todoit/static/js/runtime-main.6d515d08.js"
+  }
+]);
